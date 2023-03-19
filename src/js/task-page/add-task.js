a4949c80195262fae5fec6b06ec2dcb875145cbd
Andrii Hrytsei <andrii.hrytsei@gmail.com>
2023-03-19 15:36:09 +0200

    add html and scripts

---
+++ src/js/task-page/add-task.js
@@ -0,0 +1,42 @@
+const taskInput = document.querySelector(".task-input")
+const addTaskBtn = document.querySelector(".add-task-input")
+const taskList = document.querySelector(".task-list")
+const taskForm = document.querySelector(".task-page-form")
+
+
+const addTaskOnClick = (e) => {
+    e.preventDefault();
+    if (taskInput.value.trim() !== ""){
+        taskList.insertAdjacentHTML("afterbegin", 
+        `<li class = "task-container">
+            <input class = "task" type="text" value = "${taskInput.value}" readonly>
+            <button class = "edit" type = "button">Edit</button>
+            <button class = "delete" type = "button">Delete</button>
+        </li>`)
+        const editBtn = document.querySelector(".edit")
+        const deleteBtn = document.querySelector(".delete")
+        const task = document.querySelector(".task")
+        const taskContainer = document.querySelector(".task-container")
+        const editOnClick = () => {
+            task.removeAttribute("readonly")
+            task.focus()
+        }
+
+        const saveOnChange = () => {
+            task.setAttribute("readonly", "")
+        }
+
+        const deleteOnClick = () => {
+            taskContainer.style.display = "none"
+        }
+        
+        deleteBtn.addEventListener("click", deleteOnClick)
+        editBtn.addEventListener("click", editOnClick)
+        task.addEventListener("change", saveOnChange)
+    }
+}
+
+
+
+
+taskForm.addEventListener("submit", addTaskOnClick)
